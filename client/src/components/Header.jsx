0a0ef47d70ff191e1fe1b0572e74da921928d95f
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-05-31 10:19:38 +0300

    create the header section

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import { Button, Navbar, TextInput, NavbarLink, NavbarCollapse, NavbarToggle } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { FaMoon } from 'react-icons/fa'
+
+export default function Header() {
+    const path = useLocation().pathname;
+
+    return (
+        <Navbar className='border-b-2'>
+            <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl'>
+                <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Sanya's</span>Blog
+            </Link>
+            <form>
+                <TextInput 
+                    type='text'
+                    placeholder='Search...'
+                    rightIcon={AiOutlineSearch}
+                    className='hidden lg:inline'
+                />
+            </form>
+            <Button className='w-12 h-10 lg:hidden' color='blue' pill>
+                <AiOutlineSearch />
+            </Button>
+            <div className='flex gap-2 md:order-2'>
+                <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
+                    <FaMoon/>
+                </Button>
+                <Link to='/sign-in'>
+                    <Button color='purple'>
+                        Sign In
+                    </Button>
+                </Link>
+                <NavbarToggle />
+            </div>
+            <NavbarCollapse>
+                <NavbarLink as={Link} to="/" active={path === '/'}>
+                    Home
+                </NavbarLink>
+                <NavbarLink as={Link} to="/about" active={path === '/about'}>
+                    About
+                </NavbarLink>
+                <NavbarLink as={Link} to="/projects" active={path === '/projects'}>
+                    Projects
+                </NavbarLink>
+            </NavbarCollapse>
+        </Navbar>
+    )
+}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
-
+import { Button } from "flowbite-react";
 export default function SignIn() {
   return (
-    <div>Sign in</div>
+    <Button className="bg-gradient-to-r from-cyan-500 to-blue-500 text-white hover:bg-gradient-to-bl focus:ring-cyan-300 dark:focus:ring-cyan-800">
+        Cyan to Blue
+      </Button>
   )
 }
